5333be0e736ffda7aac41523efa929bb5e4d0311
Bilal Chaudhry <mbc000@live.com>
2021-10-12 11:54:54 +0500

    branch1: added add_string

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 def plus(x,y): # some comment 1
     return x + y
 
-<<<<<<< HEAD
+
 def mult(x,y):
 	return x * y
-=======
+
 def divide(x,y):
 	return x / y
->>>>>>> main
+
+def add_string(x, y):
+	return x + y
